b8b9fbbbd00c3b11bf7da91d9bac1735404b3cf7
Junaid Alam <joenayjoe@gmail.com>
2020-02-04 22:59:39 +0100

    added link to drop down item

--- src/components/navbar/DropDownMenu.tsx
+++ src/components/navbar/DropDownMenu.tsx
@@ -1,10 +1,4 @@
-import React, {
-  createRef,
-  useState,
-  useEffect,
-  RefObject,
-  useRef
-} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { isMobile } from "react-device-detect";
@@ -23,9 +17,11 @@ import Spinner from "../spinner/Spinner";
 import {
   BUILD_GRADE_URL,
   BUILD_COURSE_URL,
-  BUILD_CATEGORY_URL
+  BUILD_CATEGORY_URL,
+  DEFAULT_COURSE_STATUS
 } from "../../settings/Constants";
 import { CourseService } from "../../services/CourseService";
+import { Link } from "react-router-dom";
 
 interface IProps extends RouteComponentProps {
   displayName: string;
@@ -58,6 +54,20 @@ const DropDownMenu: React.FunctionComponent<IProps> = props => {
   let menuBtnNode = useRef<HTMLButtonElement>(null);
   let menuNode = useRef<HTMLUListElement>(null);
 
+  useEffect(() => {
+    setIsLoadingCategory(true);
+    categoryService.getCategories("name_asc").then(response => {
+      setCategories(response.data);
+      setIsLoadingCategory(false);
+    });
+    document.addEventListener("mousedown", e => handleOnClick(e), false);
+
+    return () => {
+      document.removeEventListener("mousedown", e => handleOnClick(e), false);
+    };
+    // eslint-disable-next-line
+  }, []);
+
   const loadPage = (item: MenuItemType) => {
     let url: string;
 
@@ -84,7 +94,8 @@ const DropDownMenu: React.FunctionComponent<IProps> = props => {
     }
   };
 
-  const handleMenuLinkClick = (item: MenuItemType) => {
+  const handleMenuLinkClick = (e: React.MouseEvent, item: MenuItemType) => {
+    e.preventDefault();
     if (isMobile) {
       if (isCourse(item)) {
         loadPage(item);
@@ -99,7 +110,7 @@ const DropDownMenu: React.FunctionComponent<IProps> = props => {
         }
         fetchCoursesForGrade(grade);
 
-        setLevelTwoParent(parent ? parent : null);
+        setLevelTwoParent(parent);
       } else {
         fetchGradesForCategory(item as ICategory);
       }
@@ -131,21 +142,23 @@ const DropDownMenu: React.FunctionComponent<IProps> = props => {
   };
   const fetchCoursesForGrade = (grade: IGrade) => {
     setIsLoadingCourse(true);
-    courseService.getCoursesByGradeId(grade.id).then(resp => {
-      let updated_cats = categories.map(cat => {
-        if (cat.id === grade.primaryCategory.id) {
-          cat.grades.map(grd => {
-            if (grd.id === grade.id) {
-              grd.courses = resp.data;
-            }
-            return grd;
-          });
-        }
-        return cat;
+    courseService
+      .getCoursesByGradeId(grade.id, DEFAULT_COURSE_STATUS)
+      .then(resp => {
+        let updated_cats = categories.map(cat => {
+          if (cat.id === grade.primaryCategory.id) {
+            cat.grades.map(grd => {
+              if (grd.id === grade.id) {
+                grd.courses = resp.data;
+              }
+              return grd;
+            });
+          }
+          return cat;
+        });
+        setCategories(updated_cats);
+        setIsLoadingCourse(false);
       });
-      setCategories(updated_cats);
-      setIsLoadingCourse(false);
-    });
   };
 
   const handleDropDownMouseEnter = () => {
@@ -172,27 +185,17 @@ const DropDownMenu: React.FunctionComponent<IProps> = props => {
     }
   };
 
-  useEffect(() => {
-    setIsLoadingCategory(true);
-    categoryService.getCategories("name_asc").then(response => {
-      setCategories(response.data);
-      setIsLoadingCategory(false);
-    });
-    document.addEventListener("mousedown", e => handleOnClick(e), false);
-
-    return () => {
-      document.removeEventListener("mousedown", e => handleOnClick(e), false);
-    };
-    // eslint-disable-next-line
-  }, []);
-
   const dropDownMenuLevelThree = (data: IGrade) => {
     let dropDownMenuItem = data.courses.map((item: ICourse) => {
       return (
         <li key={item.id} className="drop-down-list-item">
-          <div className="menu-link" onClick={() => handleMenuLinkClick(item)}>
+          <Link
+            to={BUILD_COURSE_URL(item.id)}
+            className="menu-link"
+            onClick={e => handleMenuLinkClick(e, item)}
+          >
             <span>{item.name}</span>
-          </div>
+          </Link>
         </li>
       );
     });
@@ -212,9 +215,13 @@ const DropDownMenu: React.FunctionComponent<IProps> = props => {
           </li>
         ) : null}
         <li key={data.id} className="drop-down-list-item">
-          <div className="menu-link" onClick={() => loadPage(data)}>
+          <Link
+            className="menu-link"
+            to={BUILD_GRADE_URL(data.id)}
+            onClick={() => loadPage(data)}
+          >
             <span>All {data.name}</span>
-          </div>
+          </Link>
         </li>
         {isLoadingCourse ? <Spinner /> : dropDownMenuItem}
       </ul>
@@ -243,10 +250,14 @@ const DropDownMenu: React.FunctionComponent<IProps> = props => {
           className={`drop-down-list-item ${openKlass}`}
           onMouseEnter={() => handleMenuItemMouseEnter(item)}
         >
-          <div className="menu-link" onClick={() => handleMenuLinkClick(item)}>
+          <Link
+            className="menu-link"
+            to={BUILD_GRADE_URL(item.id)}
+            onClick={e => handleMenuLinkClick(e, item)}
+          >
             <span>{item.name}</span>
             {expander}
-          </div>
+          </Link>
           {submenu}
         </li>
       );
@@ -267,9 +278,13 @@ const DropDownMenu: React.FunctionComponent<IProps> = props => {
           </li>
         ) : null}
         <li key={data.id} className="drop-down-list-item">
-          <div className="menu-link" onClick={() => loadPage(data)}>
+          <Link
+            className="menu-link"
+            to={BUILD_CATEGORY_URL(data.id)}
+            onClick={() => loadPage(data)}
+          >
             <span> All {data.name}</span>
-          </div>
+          </Link>
         </li>
         {isLoadingGrade ? <Spinner /> : dropDownMenuItem}
       </ul>
@@ -298,10 +313,14 @@ const DropDownMenu: React.FunctionComponent<IProps> = props => {
           className={`drop-down-list-item ${openKlass}`}
           onMouseEnter={() => handleMenuItemMouseEnter(item)}
         >
-          <div className="menu-link" onClick={() => handleMenuLinkClick(item)}>
+          <Link
+            className="menu-link"
+            to={BUILD_CATEGORY_URL(item.id)}
+            onClick={e => handleMenuLinkClick(e, item)}
+          >
             <span>{item.name}</span>
             {expander}
-          </div>
+          </Link>
           {submenu}
         </li>
       );
